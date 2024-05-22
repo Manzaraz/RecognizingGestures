5028d6ef93edb871855e4e42cad042e8381453e8
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-22 10:52:46 -0300

    Initial Commit

---
+++ RecognizingGestures/RecognizingGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecognizingGesturesApp.swift
+//  RecognizingGestures
+//
+//  Created by Christian Manzaraz on 22/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecognizingGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
